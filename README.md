97311632852431204e66b0a195f2a2c8e2e3d838
William <williammongeau@gmail.com>
2021-12-12 12:26:48 -0500

    README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AiProj_3
 https://github.com/wmongeau/AiProj_3.git
+
+## Instructions
+To run this project, simply uncomment a single modelName from the lines 5 to 13 in main.py.
+If you wish to run this project from "scratch", you would need to delete everything in the analysis.csv file.
+Otherwise, new results will be appended to that file. 
+Finally, simply run ```python main.py```
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import gensim.downloader as api
 import csv
+from random import randrange
 
-# modelName = "word2vec-google-news-300"
+modelName = "word2vec-google-news-300"
 
 # Same Corpora, different embeddings sizes
 # modelName = "glove-wiki-gigaword-50"
@@ -9,7 +10,7 @@ import csv
 
 # Different Corpora, same embedding sizes
 # modelName = "glove-twitter-100"
-modelName = "glove-wiki-gigaword-100"
+# modelName = "glove-wiki-gigaword-100"
 
 model = api.load(modelName)
 sizeOfVocab = len(model.index_to_key)
@@ -20,7 +21,10 @@ with open('synonyms.csv', newline='\n') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     for row in csv_reader:
         questionExists = True
-        allOptionsExist = True
+        option1Exists = True
+        option2Exists = True
+        option3Exists = True
+        option4Exists = True
         try:
             model[row["question"]]
         except KeyError:
@@ -28,27 +32,42 @@ with open('synonyms.csv', newline='\n') as csv_file:
         try:
             model[row["0"]]
         except KeyError:
-            allOptionsExist = False
+            option1Exists = False
         try:
             model[row["1"]]
         except KeyError:
-            allOptionsExist = False
+            option2Exists = False
         try:
             model[row["2"]]
         except KeyError:
-            allOptionsExist = False
+            option3Exists = False
         try:
             model[row["3"]]
         except KeyError:
-            allOptionsExist = False
+            option4Exists = False
 
         label = ""
         guess = ""
-        if questionExists and allOptionsExist:
+        s1 = -10000
+        s2 = -10000
+        s3 = -10000
+        s4 = -10000
+
+        if questionExists and option1Exists:
             s1 = model.similarity(row["question"], row["0"])
+        if questionExists and option2Exists:
             s2 = model.similarity(row["question"], row["1"])
+        if questionExists and option3Exists:
             s3 = model.similarity(row["question"], row["2"])
+        if questionExists and option4Exists:
             s4 = model.similarity(row["question"], row["3"])
+
+        if not questionExists or (not option1Exists and not option2Exists and not option3Exists and not option4Exists):
+            label = "guess"
+            guessIndex = randrange(4)
+            guess = row[str(guessIndex)]
+            guessed += 1
+        else:
             if s1 > s2 and s1 > s3 and s1 > s4:
                 guess = row["0"]
                 if row["0"] == row["answer"]:
@@ -77,12 +96,7 @@ with open('synonyms.csv', newline='\n') as csv_file:
                     correct += 1
                 else:
                     label = "wrong"
-        else:
-            label = "guess"
-            guess = "Guess"
-            guessed += 1
 
-        f.write(f'{row["question"]}, ')
         f.write(f'{row["question"]}, ')
         f.write(f'{row["answer"]}, ')
         f.write(f'{guess}, ')
